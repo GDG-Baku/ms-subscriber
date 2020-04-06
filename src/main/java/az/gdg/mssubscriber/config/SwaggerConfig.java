84b3245b2ae9e30d7bc6760282af7bef6a00175d
alihsoff <alihsoff@gmail.com>
2020-04-07 00:59:40 +0400

    BE-22: remove basic-error-controller in swagger

--- src/main/java/az/gdg/mssubscriber/config/SwaggerConfig.java
+++ src/main/java/az/gdg/mssubscriber/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("az.gdg.mssubscriber"))
                 .paths(PathSelectors.any())
                 .build();
     }
