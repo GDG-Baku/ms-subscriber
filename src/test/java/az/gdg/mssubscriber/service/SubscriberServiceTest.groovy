52af0ec798d8663f116bb74b244c8602ee206540
alihsoff <alihsoff@gmail.com>
2020-06-01 16:00:21 +0400

    BE-22: update tests

--- src/test/java/az/gdg/mssubscriber/service/SubscriberServiceTest.groovy
+++ src/test/java/az/gdg/mssubscriber/service/SubscriberServiceTest.groovy
@@ -1,8 +1,13 @@
+package az.gdg.mssubscriber.service
+
 import az.gdg.mssubscriber.exception.SubscriberAlreadyExistException
 import az.gdg.mssubscriber.mapper.SubscriberMapper
+import az.gdg.mssubscriber.model.dto.MailDTO
 import az.gdg.mssubscriber.model.dto.SubscriberDTO
 import az.gdg.mssubscriber.repository.SubscriberRepository
 import az.gdg.mssubscriber.repository.entity.SubscriberEntity
+import az.gdg.mssubscriber.security.util.TokenUtil
+import az.gdg.mssubscriber.service.impl.MailServiceImpl
 import az.gdg.mssubscriber.service.impl.SubscriberServiceImpl
 import spock.lang.Specification
 import spock.lang.Title
@@ -12,10 +17,14 @@ class SubscriberServiceTest extends Specification {
     
     SubscriberRepository subscriberRepository
     SubscriberServiceImpl subscriberServiceImpl
+    MailServiceImpl mailServiceImpl
+    TokenUtil tokenUtil
     
     def setup() {
         subscriberRepository = Mock()
-        subscriberServiceImpl = new SubscriberServiceImpl(subscriberRepository)
+        mailServiceImpl = Mock()
+        tokenUtil = Mock()
+        subscriberServiceImpl = new SubscriberServiceImpl(subscriberRepository, mailServiceImpl, tokenUtil)
     }
     
     def "should use the repository to fetch all subscribers"() {
@@ -36,12 +45,20 @@ class SubscriberServiceTest extends Specification {
         given:
             def subscriberDTO = new SubscriberDTO("alihsoff@gmail.com")
             def subscriber = SubscriberMapper.INSTANCE.dtoToEntity(subscriberDTO)
+            def mailDTO = new MailDTO()
+            mailDTO.to = Collections.singletonList(subscriber.email)
+            mailDTO.subject = "You have been subscribed"
+            tokenUtil.generateTokenWithEmail(subscriberDTO.getEmail()) >> null
+            mailDTO.body = "Congrats! You have been subscribed successfully<br>" +
+                    "<a href='http://http://gdg-ms-auth.herokuapp.com/subscriber/unsubscribe?token=" + tokenUtil.generateTokenWithEmail(subscriberDTO.getEmail()) + "'>Unsubscribe</a>"
+            subscriberRepository.findSubscriberByEmail(subscriberDTO.getEmail()) >> null
         
         when:
             subscriberServiceImpl.createSubscriber(subscriberDTO)
         
         then:
             1 * subscriberRepository.save(subscriber)
+            1 * mailServiceImpl.sendToQueue(mailDTO)
             notThrown(SubscriberAlreadyExistException)
     }
     
@@ -60,16 +77,17 @@ class SubscriberServiceTest extends Specification {
     
     def "should delete subscriber by email"() {
         given:
+            def token = "asasdasfsdgsdf"
             def email = "alihsoff@gmail.com"
             def subscriber = new SubscriberEntity(id: 1, email: email)
         
         
         when:
-            subscriberServiceImpl.deleteSubscriber(email)
+            subscriberServiceImpl.deleteSubscriber(token)
         
         then:
+            1 * tokenUtil.getEmailFromToken(token) >> email
             1 * subscriberRepository.findSubscriberByEmail(email) >> subscriber
             1 * subscriberRepository.delete(subscriber)
-        
     }
 }
