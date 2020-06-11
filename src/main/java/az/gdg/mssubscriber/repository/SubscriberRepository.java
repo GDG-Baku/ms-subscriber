a6a807cecb46d7504f578e14bdeb698e0ce21a2c
alihsoff <alihsoff@gmail.com>
2020-06-11 22:51:47 +0400

    BE-22: change ID type

--- src/main/java/az/gdg/mssubscriber/repository/SubscriberRepository.java
+++ src/main/java/az/gdg/mssubscriber/repository/SubscriberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubscriberRepository extends JpaRepository<SubscriberEntity, Integer> {
+public interface SubscriberRepository extends JpaRepository<SubscriberEntity, Long> {
     SubscriberEntity findSubscriberByEmail(String email);
 
 }
